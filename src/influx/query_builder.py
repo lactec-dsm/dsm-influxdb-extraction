40aaa7b9c9c1272e52aa03157847dfa5cdb6ab21
Leandro Mario <leandro.mario@lactec.com.br>
2026-03-23 11:24:34 -0300

    Alteração no query bulider para padronização das colunas que são usadas como parâmetro na consulta

--- src/influx/query_builder.py
+++ src/influx/query_builder.py
@@ -1,6 +1,26 @@
-def build_query(measurement_id, device_Id, start, end, fields="*", limit=1000000):
-    return (
-        f'SELECT {fields} FROM "{measurement_id}" '
-        f'WHERE "deviceId" = \'{device_Id}\' AND time >= \'{start}\' AND time <= \'{end}\' '
-        f'ORDER BY time ASC '
+def build_query(
+    measurement_id: str,
+    device_id: str,
+    start: str,
+    end: str,
+) -> str:
+    """
+    Monta a query de extracao no InfluxDB.
+
+    Parametros internos do projeto:
+    - measurement_id
+    - device_id
+
+    Nome real da tag no Influx:
+    - deviceId
+    """
+    query = (
+        f'SELECT * '
+        f'FROM "{measurement_id}" '
+        f'WHERE "deviceId" = \'{device_id}\' '
+        f"AND time >= '{start}' "
+        f"AND time <= '{end}' "
+        f'ORDER BY time ASC'
     )
+
+    return query
